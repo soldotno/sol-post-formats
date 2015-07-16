2df54be4972c1162dbf2ccbc987d264e855b3232
misund <thomas@hemmeligadresse.com>
2015-07-16 15:12:20 +0200

    Changes in code style.

--- sol-post-formats.php
+++ sol-post-formats.php
@@ -21,55 +21,52 @@ class SOL_Post_Formats {
 
 	public function load(){
 		// Register taxonomy
-		add_action( 'init', array(__CLASS__,'register_taxonomy'));
+		add_action( 'init' , array( __CLASS__ , 'register_taxonomy' ) );
 
-		// Remove old taxonomy meta box  
-		add_action( 'admin_menu', array(__CLASS__,'remove_meta_box'));  
+		// Remove old taxonomy meta box
+		add_action( 'admin_menu' , array( __CLASS__ , 'remove_meta_box' ) );
 
-		// Add new taxonomy meta box  
-		add_action( 'add_meta_boxes', array(__CLASS__,'add_meta_box'));  
-
-		// Load admin scripts
-		add_action('admin_enqueue_scripts',array(__CLASS__,'admin_script'));
+		// Add new taxonomy meta box
+		add_action( 'add_meta_boxes' , array( __CLASS__ , 'add_meta_box' ) );
 
 		// Load admin scripts
-		add_action('wp_ajax_radio_tax_add_taxterm',array(__CLASS__,'ajax_add_term'));
+		add_action( 'admin_enqueue_scripts' , array( __CLASS__ , 'admin_script' ) );
+
+		// Load admin scripts
+		add_action( 'wp_ajax_radio_tax_add_taxterm' , array( __CLASS__ , 'ajax_add_term' ) );
 
 		// Add class names without prefix to post_class()
-		add_action('post_class', array(__CLASS__,'modify_post_class'));
+		add_action( 'post_class' , array( __CLASS__ , 'modify_post_class' ) );
 
 		// Add class name to TinyMCE
-		add_action('tiny_mce_before_init', array(__CLASS__, 'modify_editor_class') , 10 , 2 );
+		add_action( 'tiny_mce_before_init' , array(__CLASS__ , 'modify_editor_class') , 10 , 2 );
 	}
 
-
-	public static function remove_meta_box(){  
-		remove_meta_box(static::$taxonomy_metabox_id, static::$post_type, 'normal');  
-	} 
-
+	public static function remove_meta_box() {
+		remove_meta_box( static::$taxonomy_metabox_id, static::$post_type, 'normal');
+	}
 
 	public function add_meta_box() {
-		add_meta_box( 'customtagsdiv-sol_post_format', __( 'Format' ), array(__CLASS__,'metabox'), static::$post_type ,'side','core');  
-	}  
+		add_meta_box( 'customtagsdiv-sol_post_format', __( 'Format' ), array(__CLASS__,'metabox'), static::$post_type ,'side','core');
+	}
 
-
-	//Callback to set up the metabox  
-	public static function metabox( $post ) {  
-		//Get taxonomy and terms  
+	// Callback to set up the metabox
+	public static function metabox( $post ) {
+		// Get taxonomy and terms
 		$taxonomy = self::$taxonomy;
 
-		//Set up the taxonomy object and get terms  
-		$tax = get_taxonomy($taxonomy);  
-		$terms = get_terms($taxonomy,array('hide_empty' => 0));  
+		// Set up the taxonomy object and get terms
+		$tax = get_taxonomy($taxonomy);
+		$terms = get_terms($taxonomy,array('hide_empty' => 0));
 
-		//Name of the form  
-		$name = 'tax_input[' . $taxonomy . ']';  
+		// Name of the form
+		$name = 'tax_input[' . $taxonomy . ']';
 
-		//Get current and popular terms  
-		$popular = get_terms( $taxonomy, array( 'orderby' => 'count', 'order' => 'DESC', 'number' => 10, 'hierarchical' => false ) );  
-		$postterms = get_the_terms( $post->ID,$taxonomy );  
-		$current = ($postterms ? array_pop($postterms) : false);  
-		$current = ($current ? $current->term_id : 0);  
+		// Get current and popular terms
+		$popular = get_terms( $taxonomy, array( 'orderby' => 'count', 'order' => 'DESC', 'number' => 10, 'hierarchical' => false ) );
+		$postterms = get_the_terms( $post->ID,$taxonomy );
+		$current = ($postterms ? array_pop($postterms) : false);
+		$current = ($current ? $current->term_id : 0);
 ?>
 
 			<div id="taxonomy-<?php echo $taxonomy; ?>" class="categorydiv">
@@ -84,26 +81,30 @@ class SOL_Post_Formats {
 				<!-- Display taxonomy terms -->
 				<div id="<?php echo $taxonomy; ?>-all" class="tabs-panel">
 					<ul id="<?php echo $taxonomy; ?>checklist" class="list:<?php echo $taxonomy?> categorychecklist form-no-clear">
-<?php foreach($terms as $term){
-$id = (is_taxonomy_hierarchical($taxonomy)) ? $taxonomy . '-' . $term->term_id : $taxonomy . '-' . $term->slug;
-$value= (is_taxonomy_hierarchical($taxonomy) ? "value='{$term->term_id}'" : "value='{$term->slug}'");
-echo "<li id='$id'><label class='selectit'>";
-echo "<input type='radio' id='in-$id' name='{$name}'".checked($current,$term->term_id,false)." {$value} />$term->name<br />";
-echo "</label></li>";
-		}?>
+						<?php
+						foreach( $terms as $term ){
+							$id = ( is_taxonomy_hierarchical( $taxonomy ) ) ? $taxonomy . '-' . $term->term_id : $taxonomy . '-' . $term->slug;
+							$value = ( is_taxonomy_hierarchical( $taxonomy ) ? "value='{$term->term_id}'" : "value='{$term->slug}'");
+							echo "<li id='$id'><label class='selectit'>";
+							echo "<input type='radio' id='in-$id' name='{$name}'".checked( $current, $term->term_id, false)." {$value} />$term->name<br />";
+							echo "</label></li>";
+						}
+						?>
 					</ul>
 				</div>
 
 				<!-- Display popular taxonomy terms -->
 				<div id="<?php echo $taxonomy; ?>-pop" class="tabs-panel" style="display: none;">
 					<ul id="<?php echo $taxonomy; ?>checklist-pop" class="categorychecklist form-no-clear" >
-<?php foreach($popular as $term){
-$id = 'popular-'.$taxonomy.'-'.$term->term_id;
-$value= (is_taxonomy_hierarchical($taxonomy) ? "value='{$term->term_id}'" : "value='{$term->slug}'");
-echo "<li id='$id'><label class='selectit'>";
-echo "<input type='radio' id='in-$id'".checked($current,$term->term_id,false)." {$value} />$term->name<br />";
-echo "</label></li>";
-}?>
+						<?php
+						foreach( $popular as $term ) {
+							$id = 'popular-'.$taxonomy.'-'.$term->term_id;
+							$value = ( is_taxonomy_hierarchical( $taxonomy ) ? "value='{$term->term_id}'" : "value='{$term->slug}'");
+							echo "<li id='$id'><label class='selectit'>";
+							echo "<input type='radio' id='in-$id'".checked( $current, $term->term_id, false)." {$value} />$term->name<br />";
+							echo "</label></li>";
+						}
+						?>
 					</ul>
 				</div>
 
@@ -113,43 +114,47 @@ echo "</label></li>";
 					<input type="button" id="" class="radio-tax-add button" value="<?php echo esc_attr( $tax->labels->add_new_item ); ?>" tabindex="3" />
 				</p>
 		  </div>
-<?php  
+<?php
 	}
 
-	public function admin_script(){  
-		wp_register_script( 'radiotax', plugins_url( '/js/radiotax.js', __FILE__ ), array('jquery'), null, true ); // We specify true here to tell WordPress this script needs to be loaded in the footer  
-		wp_localize_script( 'radiotax', 'radio_tax', array('slug'=>self::$taxonomy));
-		wp_enqueue_script( 'radiotax' );  
+	public function admin_script() {
+		wp_register_script( 'radiotax' , plugins_url( '/js/radiotax.js' ,  __FILE__ ) , array( 'jquery' ) , null , true ); // We specify true here to tell WordPress this script needs to be loaded in the footer
+		wp_localize_script( 'radiotax' , 'radio_tax' , array( 'slug' => self::$taxonomy ) );
+		wp_enqueue_script( 'radiotax' );
 	}
 
-	public function ajax_add_term(){
+	public function ajax_add_term() {
 
-		$taxonomy = !empty($_POST['taxonomy']) ? $_POST['taxonomy'] : '';
-		$term = !empty($_POST['term']) ? $_POST['term'] : '';
-		$tax = get_taxonomy($taxonomy);
+		$taxonomy = !empty( $_POST['taxonomy'] ) ? $_POST['taxonomy'] : '';
+		$term = !empty( $_POST['term'] ) ? $_POST['term'] : '';
+		$tax = get_taxonomy( $taxonomy );
 
-		check_ajax_referer('radio-tax-add-'.$taxonomy, '_wpnonce_radio-add-tag');
+		check_ajax_referer( 'radio-tax-add-' . $taxonomy, '_wpnonce_radio-add-tag' );
 
-		if(!$tax || empty($term))
+		if( !$tax || empty( $term ) )
 			exit();
 
 		if ( !current_user_can( $tax->cap->edit_terms ) )
-			die('-1');
+			die( '-1' );
 
-		$tag = wp_insert_term($term, $taxonomy);
+		$tag = wp_insert_term( $term, $taxonomy );
 
-		if ( !$tag || is_wp_error($tag) || (!$tag = get_term( $tag['term_id'], $taxonomy )) ) {
+		if ( !$tag || is_wp_error( $tag ) || ( !$tag = get_term( $tag['term_id'], $taxonomy ) ) ) {
 			//TODO Error handling
 			exit();
 		}
 
 		$id = $taxonomy.'-'.$tag->term_id;
 		$name = 'tax_input[' . $taxonomy . ']';
-		$value= (is_taxonomy_hierarchical($taxonomy) ? "value='{$tag->term_id}'" : "value='{$term->tag_slug}'");
+		$value = ( is_taxonomy_hierarchical( $taxonomy ) ? "value='{$tag->term_id}'" : "value='{$term->tag_slug}'");
 
-		$html = '<li id="'.$id.'"><label class="selectit"><input type="radio" id="in-'.$id.'" name="'.$name.'" '.$value.' />'. $tag->name.'</label></li>';
+		$html = '<li id="' . $id . '"><label class="selectit"><input type="radio" id="in-' . $id . '" name="' . $name . '" ' . $value . ' />' . $tag->name . '</label></li>';
 
-		echo json_encode(array('term'=>$tag->term_id,'html'=>$html));
+		echo json_encode( array(
+			'term' => $tag->term_id,
+			'html'=>$html
+		) );
+
 		exit();
 	}
 
