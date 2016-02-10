a20c50c12bd0271bd6bc0941a750651b5a254d8e
misund <thomas@hemmeligadresse.com>
2016-02-10 18:23:20 +0100

    Fix bug when post did not have a sol post format

--- template-tags.php
+++ template-tags.php
@@ -5,8 +5,11 @@ function get_sol_post_format() {
 	if ( !$post || !$post->ID )
 		return false;
 
-	foreach ( get_the_terms( $post->ID, 'sol_post_format' ) as $term )
-		return $term->slug;
+	$terms = get_the_terms( $post->ID, 'sol_post_format' );
+
+	if ( $terms )
+		foreach ( $terms as $term )
+			return $term->slug;
 
 	return false;
 
